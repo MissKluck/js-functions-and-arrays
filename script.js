2d6118ac93a616dce6e9676d924f9546c8f1c51d
misskluck <sigrid.b.kluck@gmail.com>
2024-05-03 11:49:20 +0200

    set up function for generating person cards

---
+++ script.js
@@ -0,0 +1,39 @@
+function createPersonCard(person) {
+  // Generere et html element
+  const articleElement = document.createElement("article");
+
+  // Lage alle elementene
+  const firstName = document.createElement("p");
+  const ageElement = document.createElement("p");
+  const profileLinkElement = document.createElement("a");
+
+  // Legg til innholdet i elementene
+  articleElement.className = "person-card";
+  firstName.textContent = sigrid.Name;
+  ageElement.textContent = sigrid.Age;
+  profileLinkElement.textContent = "Profile";
+  profileLinkElement.href = sigrid.homePage;
+
+  // Lagt de til containeren
+  articleElement.appendChild(firstName);
+  articleElement.appendChild(ageElement);
+  articleElement.appendChild(profileLinkElement);
+
+  return articleElement;
+}
+
+// Hvilken data har vi
+const sigrid = {
+  Name: "Sigrid",
+  Age: 28,
+  homePage: "http://www.github.com/misskluck",
+};
+
+// Kall funksjonen med persons objektet vårt
+const sigridElement = createPersonCard(sigrid);
+
+// Sette inn elementet i dokumentet
+// 1. Finn ut hvor vi skal sette elementet inn
+const container = document.getElementById("single-person");
+// 2. Sett inn det nye elementet som et underelement
+container.appendChild(sigridElement);
